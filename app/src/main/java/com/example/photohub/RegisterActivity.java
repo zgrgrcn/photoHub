fc7a33fb562a2cc87c91be693faba42589d9f252
ozgur <ozgurgurcan1996@gmail.com>
2020-03-09 22:09:36 +0300

    Main->RegisterActivity firebase->First user created

---
+++ app/src/main/java/com/example/photohub/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.example.photohub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    EditText emailText,passwordText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        emailText=findViewById(R.id.emailText);
+        passwordText=findViewById(R.id.passwordText);
+        //FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+    }
+
+    public void registerClicked(View view){
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(RegisterActivity.this,"User Created !",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+    public void loginClicked(View view){
+
+    }
+}
