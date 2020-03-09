e36ecff95bba008fbe63323a20eb2d27ee92ec4e
ozgur <ozgurgurcan1996@gmail.com>
2020-03-10 01:03:05 +0300

    logout, add image menu auto login

---
+++ app/src/main/java/com/example/photohub/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.photohub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.addPost){
+            Intent intentToUpload = new Intent(MainActivity.this,UploadActivity.class);
+            startActivity(intentToUpload);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance();
+    }
+
+    public void logOut(View view){
+        firebaseAuth.signOut();
+        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
