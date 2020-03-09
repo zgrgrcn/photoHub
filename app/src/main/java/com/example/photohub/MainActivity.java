94c91a0551b4577752558457b079e2fae83d26e9
ozgur <ozgurgurcan1996@gmail.com>
2020-03-09 21:34:47 +0300

    mail password text and button

--- app/src/main/java/com/example/photohub/MainActivity.java
+++ app/src/main/java/com/example/photohub/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.photohub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void registerClicked(View view){
+
+    }
+
+    public void loginClicked(View view){
 
     }
 }
